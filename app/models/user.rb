24d047c917fd7d8407e535ad03b0f6a528de5fd5
qdtrl <quentindtouroul@gmail.com>
2021-03-02 18:55:10 +0100

    correct validation + added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
 		:jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
-  validates :title,
+  validates :email,
 		presence: true
 
   has_many :articles
